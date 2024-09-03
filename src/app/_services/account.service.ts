75c6da2d61e0946ffa22ed56cd3898b0bf5a974f
Rekha Rana <rrana@inrsenterprises.com>
2024-09-03 11:12:15 +0530

    End of section 5

---
+++ src/app/_services/account.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { map, single } from 'rxjs';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+ private http = inject(HttpClient)
+baseurl="https://localhost:5001/api/";
+currentUser= signal<User|null>(null);
+login(model:any){
+  return this.http.post<User>(this.baseurl+'account/login',model).pipe(
+    map(user=>{
+      if(user){
+          localStorage.setItem('user',JSON.stringify(user));
+          this.currentUser.set(user);
+      } 
+    })
+  )
+ }
+ register(model:any){
+  return this.http.post<User>(this.baseurl+'account/register',model).pipe(
+    map(user=>{
+      if(user){
+          localStorage.setItem('user',JSON.stringify(user));
+          this.currentUser.set(user);
+      } 
+      return user;
+    })
+  )
+ }
+ logout(){
+  localStorage.removeItem('user');
+  this.currentUser.set(null);
+ }
+}
+
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,32 @@
 
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { RouterOutlet ,RouterLink} from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
+import { RegisterComponent } from './register/register.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, CommonModule, NgbModule, RouterLink, HomeComponent,RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   title = 'DatingApp';
-  http= inject(HttpClient);
- 
+  accountService= inject(AccountService); 
 users:any;
 
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/users')
-    .subscribe({
-      
-      next:response=>this.users=response,
-      error: error=>console.error(),     
-      complete:()=>console.log('Request has been completed')
-    });
+  
+  setCurrentUser(){
+    const userString= localStorage.getItem('user');
+    if(!userString) return;
+
+    const user= JSON.parse(userString);
+    this.accountService.currentUser.set(user);
   }
- 
+
 }
