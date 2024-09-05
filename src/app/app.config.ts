aeb462bd26e4328af9cdf7e81fd5c6740982442f
Rekha Rana <rrana@inrsenterprises.com>
2024-09-05 16:21:43 +0530

    End Of section 7

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,15 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations'
 import { provideToastr } from 'ngx-toastr';
+import { errorInterceptor } from './_interceptor/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideHttpClient(withInterceptors([errorInterceptor])),
     provideHttpClient(),
     provideAnimations(),
     provideToastr({
